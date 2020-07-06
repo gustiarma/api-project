596c51b6ccf78765f023cde69f38c58e07f1e36d
Rendrian Arma <rendrian@gustiarma.com>
2020-07-06 20:48:12 +0700

    add few  routing on joox api

--- app/Http/Controllers/JooxAPIController.php
+++ app/Http/Controllers/JooxAPIController.php
@@ -11,10 +11,71 @@ class JooxAPIController extends Controller
     public function index($command)
     {
         switch ($command) {
-            case 'searchSong':
-                $kw = str_replace('%20', '', $_GET['keyword']);
+            case 'search':
 
-                return JooxAPIx::songByKeyword($kw);
+                if (!isset($_GET['keyword'])) {
+                    return response([
+                        'success' => false,
+                        'msg' => 'Please provide keyword parameter , and read docs!'
+                    ]);
+                }
+                $kw = str_replace('%20', '', $_GET['keyword']);
+                $data = JooxAPIx::songByKeyword($kw);
+                return response([
+                    'success' => true,
+                    'data' => $data
+                ]);
+
+                break;
+            case 'details':
+
+                if (!isset($_GET['id'])) {
+                    return response([
+                        'success' => false,
+                        'msg' => 'Please provide keyword parameter , and read docs!'
+                    ]);
+                }
+                $songId = $_GET['id'];
+                $data = JooxAPIx::songById($songId);
+                if(is_iterable($data)){
+                    $data = $data[0];
+                }
+                return response([
+                    'success' => true,
+                    'data' => $data
+                ]);
+
+
+                break;
+            case 'album':
+                if (!isset($_GET['id'])) {
+                    return response([
+                        'success' => false,
+                        'msg' => 'Please provide keyword parameter , and read docs!'
+                    ]);
+                }
+
+                $albumId = $_GET['id'];
+                $data =  JooxAPIx::songByAlbum($albumId);
+                return response([
+                    'success' => true,
+                    'data' => $data
+                ]);
+
+                break;
+            case 'downloadSong':
+
+                if (!isset($_GET['id'])) {
+                    return response([
+                        'success' => false,
+                        'msg' => 'Please provide keyword parameter , and read docs!'
+                    ]);
+                }
+                $id = $_GET['id'];
+                $type = (!isset($_GET['type'])) ? 'mp3' : $_GET['type'];
+
+                return JooxAPIx::downloadSong($id, $type);
+
 
 
                 break;
